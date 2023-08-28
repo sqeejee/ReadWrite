d515ce90ce46cf2d90031ac78b5211c6ca4fb738
sam <baconsamp@gmail.com>
2023-08-28 10:59:58 -0700

    Fixed files
    Code works

--- readwrite.java
+++ readwrite.java
@@ -3,26 +3,43 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.*;
 
-import javax.imageio.IIOException;
-
-//reads a file
-
-public class readwrite {
-    public readwrite(String inputFile) throws IOException {
+// Reads a file
+public class readWrite {
+    public readWrite(String inputFile) throws IOException {
         File file = new File(inputFile);
         BufferedReader reader = new BufferedReader(new FileReader(file));
-        while ((reader.read()) != -1) {
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
         }
         reader.close();
     }
 
     // Asks for a file and a text string, writes the string into the file
-
     public void writeCode(String codeFile, String text) throws IOException {
-        PrintWriter writer = new PrintWriter(codeFile);
-        writer.println(text);
-        writer.close();
+        try (PrintWriter writer = new PrintWriter(codeFile)) {
+            writer.println(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-}
+
+    public static void main(String[] args) {
+        try {
+            readWrite rw = new readWrite("input.txt");
+            rw.writeCode("output.txt", "aidan rahill is my baby boy i love him");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }}
+
+    
+
+    
+
+    
+
+
+
+
