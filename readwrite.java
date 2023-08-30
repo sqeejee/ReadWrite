e15082f4dae2ac915808467a7fd21c00e0be7ca1
aidanrahill <116307633+aidanrahill@users.noreply.github.com>
2023-08-30 11:34:18 -0700

    Fixed Write and added CountCharacters
    vscode was bitching about somehting so I changed some stuff and fixed your write to return a string instead of printing to console

--- readwrite.java
+++ readwrite.java
@@ -5,19 +5,21 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 // Reads a file
-public class readWrite {
-    public readWrite(String inputFile) throws IOException {
+public class readwrite {
+    public String read(String inputFile) throws IOException {
+        String str = "";
         File file = new File(inputFile);
         BufferedReader reader = new BufferedReader(new FileReader(file));
         String line;
         while ((line = reader.readLine()) != null) {
-            System.out.println(line);
+            str += line;
         }
         reader.close();
+        return str;
     }
 
     // Asks for a file and a text string, writes the string into the file
-    public void writeCode(String codeFile, String text) throws IOException {
+    public void write(String codeFile, String text) throws IOException {
         try (PrintWriter writer = new PrintWriter(codeFile)) {
             writer.println(text);
         } catch (IOException e) {
@@ -25,21 +27,21 @@ public class readWrite {
         }
     }
 
-    public static void main(String[] args) {
+    // public static void main(String[] args) {
+    // try {
+    // readwrite rw = new readwrite("input.txt");
+    // rw.writeCode("output.txt", "aidan rahill is my baby boy i love him");
+    // } catch (IOException e
+
+    public int countCharacters(String fileName) throws IOException {
+        int count;
         try {
-            readWrite rw = new readWrite("input.txt");
-            rw.writeCode("output.txt", "aidan rahill is my baby boy i love him");
+            count = read(fileName).length();
         } catch (IOException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
+            count = 0;
         }
-    }}
-
-    
-
-    
-
-    
-
-
-
-
+        return count;
+    }
+}
